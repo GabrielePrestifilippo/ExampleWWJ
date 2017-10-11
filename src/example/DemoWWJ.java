6763cc47cb58eceb11d9b445634ae19da8adb905
GabrielePrestifilippo <gabry.501@hotmail.it>
2017-10-11 18:23:19 +0200

    update example

--- src/example/DemoWWJ.java
+++ src/example/DemoWWJ.java
@@ -6,22 +6,27 @@
 package example;
 
 import gov.nasa.worldwind.*;
-import gov.nasa.worldwind.avlist.AVKey;
+import gov.nasa.worldwind.avlist.*;
 import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
+import gov.nasa.worldwind.data.*;
 import gov.nasa.worldwind.event.*;
 import gov.nasa.worldwind.exception.WWAbsentRequirementException;
 import gov.nasa.worldwind.geom.Sector;
 import gov.nasa.worldwind.globes.*;
 import gov.nasa.worldwind.layers.*;
 
+import gov.nasa.worldwind.render.SurfaceImage;
 import gov.nasa.worldwind.terrain.*;
 import gov.nasa.worldwind.util.*;
-import gov.nasa.worldwindx.examples.dataimport.ImportElevations;
+import gov.nasa.worldwindx.examples.dataimport.*;
 import gov.nasa.worldwindx.examples.util.*;
 
 import javax.swing.*;
+import javax.swing.event.*;
 import java.awt.*;
+import java.awt.image.*;
 import java.io.File;
+import java.net.URISyntaxException;
 
 import gov.nasa.worldwindx.examples.*;
 
@@ -92,7 +97,11 @@ public class DemoWWJ
         protected LayerPanel layerPanel;
         protected StatisticsPanel statsPanel;
 
-        protected static final String ELEVATIONS_PATH = "gov/nasa/worldwindx/examples/data/craterlake-elev-16bit-30m.tif";
+        // The elevation to import
+        protected static final String ELEVATIONS_PATH
+            = "gov/nasa/worldwindx/examples/data/craterlake-elev-16bit-30m.tif";
+        // The Imagery to import
+        protected static final String IMAGE_PATH = "gov/nasa/worldwindx/examples/data/craterlake-imagery-30m.tif";
 
         protected void importElevations()
         {
@@ -133,9 +142,151 @@ public class DemoWWJ
             }
         }
 
+        protected void importImagery()
+        {
+            try
+            {
+                // Read the data and save it in a temp file.
+                File sourceFile = ExampleUtil.saveResourceToTempFile(IMAGE_PATH, ".tif");
+
+                // Create a raster reader to read this type of file. The reader is created from the currently
+                // configured factory. The factory class is specified in the Configuration, and a different one can be
+                // specified there.
+                DataRasterReaderFactory readerFactory
+                    = (DataRasterReaderFactory) WorldWind.createConfigurationComponent(
+                    AVKey.DATA_RASTER_READER_FACTORY_CLASS_NAME);
+                DataRasterReader reader = readerFactory.findReaderFor(sourceFile, null);
+
+                // Before reading the raster, verify that the file contains imagery.
+                AVList metadata = reader.readMetadata(sourceFile, null);
+                if (metadata == null || !AVKey.IMAGE.equals(metadata.getStringValue(AVKey.PIXEL_FORMAT)))
+                    throw new Exception("Not an image file.");
+
+                // Read the file into the raster. read() returns potentially several rasters if there are multiple
+                // files, but in this case there is only one so just use the first element of the returned array.
+                DataRaster[] rasters = reader.read(sourceFile, null);
+                if (rasters == null || rasters.length == 0)
+                    throw new Exception("Can't read the image file.");
+
+                DataRaster raster = rasters[0];
+
+                // Determine the sector covered by the image. This information is in the GeoTIFF file or auxiliary
+                // files associated with the image file.
+                final Sector sector = (Sector) raster.getValue(AVKey.SECTOR);
+                if (sector == null)
+                    throw new Exception("No location specified with image.");
+
+                // Request a sub-raster that contains the whole image. This step is necessary because only sub-rasters
+                // are reprojected (if necessary); primary rasters are not.
+                int width = raster.getWidth();
+                int height = raster.getHeight();
+
+                // getSubRaster() returns a sub-raster of the size specified by width and height for the area indicated
+                // by a sector. The width, height and sector need not be the full width, height and sector of the data,
+                // but we use the full values of those here because we know the full size isn't huge. If it were huge
+                // it would be best to get only sub-regions as needed or install it as a tiled image layer rather than
+                // merely import it.
+                DataRaster subRaster = raster.getSubRaster(width, height, sector, null);
+
+                // Tne primary raster can be disposed now that we have a sub-raster. Disposal won't affect the
+                // sub-raster.
+                raster.dispose();
+
+                // Verify that the sub-raster can create a BufferedImage, then create one.
+                if (!(subRaster instanceof BufferedImageRaster))
+                    throw new Exception("Cannot get BufferedImage.");
+                BufferedImage image = ((BufferedImageRaster) subRaster).getBufferedImage();
+
+                // The sub-raster can now be disposed. Disposal won't affect the BufferedImage.
+                subRaster.dispose();
+
+                // Create a SurfaceImage to display the image over the specified sector.
+                final SurfaceImage si1 = new SurfaceImage(image, sector);
+
+                // On the event-dispatch thread, add the imported data as an SurfaceImageLayer.
+                SwingUtilities.invokeLater(new Runnable()
+                {
+                    public void run()
+                    {
+                        // Add the SurfaceImage to a layer.
+                        SurfaceImageLayer layer = new SurfaceImageLayer();
+                        layer.setName("Imported Surface Image");
+                        layer.setPickEnabled(false);
+                        layer.addRenderable(si1);
+
+                        // Add the layer to the model and update the application's layer panel.
+                        insertBeforeCompass(DemoWWJ.AppFrame.this.getWwd(), layer);
+
+                        // Set the view to look at the imported image.
+                        ExampleUtil.goTo(getWwd(), sector);
+                    }
+                });
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        protected final Dimension wcsPanelSize = new Dimension(400, 600);
+        protected JTabbedPane tabbedPane;
+        protected int previousTabIndex;
         public AppFrame()
         {
+
+            this.tabbedPane = new JTabbedPane();
+
+            this.tabbedPane.add(new JPanel());
+            this.tabbedPane.setTitleAt(0, "+");
+            this.tabbedPane.addChangeListener(new ChangeListener()
+            {
+                public void stateChanged(ChangeEvent changeEvent)
+                {
+                    if (tabbedPane.getSelectedIndex() != 0)
+                    {
+                        previousTabIndex = tabbedPane.getSelectedIndex();
+                        return;
+                    }
+
+                    String server = JOptionPane.showInputDialog("Enter WCS server URL");
+                    if (server == null || server.length() < 1)
+                    {
+                        tabbedPane.setSelectedIndex(previousTabIndex);
+                        return;
+                    }
+
+                    // Respond by adding a new WMSLayerPanel to the tabbed pane.
+                    if (addTab(tabbedPane.getTabCount(), server.trim()) != null)
+                        tabbedPane.setSelectedIndex(tabbedPane.getTabCount() - 1);
+                }
+            });
             this.initialize(true, true, false);
+
+            /**
+             * ADD SERVERS
+             */
+            final String[] servers = new String[]
+                {
+                    "http://localhost:8080/geoserver/ows?service=WCS&version=2.0.1&request=GetCapabilities",
+                };
+
+            for (int i = 0; i < servers.length; i++)
+            {
+                this.addTab(i + 1, servers[i]); // i+1 to place all server tabs to the right of the Add Server tab
+            }
+
+            // Display the first server pane by default.
+            this.tabbedPane.setSelectedIndex(this.tabbedPane.getTabCount() > 0 ? 1 : 0);
+            this.previousTabIndex = this.tabbedPane.getSelectedIndex();
+
+            // Add the tabbed pane to a frame separate from the world window.
+            JFrame controlFrame = new JFrame();
+            controlFrame.getContentPane().add(tabbedPane);
+            controlFrame.pack();
+            controlFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            controlFrame.setVisible(true);
+            /**
+             * ADD SERVER
+             */
             this.setCursor(new Cursor(Cursor.WAIT_CURSOR));
 
             // Import the elevations on a thread other than the event-dispatch thread to avoid freezing the UI.
@@ -143,7 +294,8 @@ public class DemoWWJ
             {
                 public void run()
                 {
-                    importElevations();
+                    // importElevations();
+                    importImagery();
 
                     // Restore the cursor.
                     setCursor(Cursor.getDefaultCursor());
@@ -153,8 +305,11 @@ public class DemoWWJ
             t.start();
         }
 
+
+
         public AppFrame(Dimension size)
         {
+
             this.canvasSize = size;
             this.initialize(true, true, false);
         }
@@ -274,20 +429,26 @@ public class DemoWWJ
             return statsPanel;
         }
 
-        public void setToolTipController(ToolTipController controller)
+        public WCSCoveragePanel addTab(int position, String server)
         {
-            if (this.wwjPanel.toolTipController != null)
-                this.wwjPanel.toolTipController.dispose();
+            final Dimension wcsPanelSize = new Dimension(400, 600);
+            // Add a server to the tabbed dialog.
+            try
+            {
+                WCSCoveragePanel coveragePanel = new WCSCoveragePanel(DemoWWJ.AppFrame.this.getWwd(), server,
+                    wcsPanelSize);
+                this.tabbedPane.add(coveragePanel, BorderLayout.CENTER);
+                this.tabbedPane.setTitleAt(position, "Geoserver Elevation");
 
-            this.wwjPanel.toolTipController = controller;
-        }
-
-        public void setHighlightController(HighlightController controller)
-        {
-            if (this.wwjPanel.highlightController != null)
-                this.wwjPanel.highlightController.dispose();
-
-            this.wwjPanel.highlightController = controller;
+                return coveragePanel;
+            }
+            catch (URISyntaxException e)
+            {
+                JOptionPane.showMessageDialog(null, "Server URL is invalid", "Invalid Server URL",
+                    JOptionPane.ERROR_MESSAGE);
+                tabbedPane.setSelectedIndex(previousTabIndex);
+                return null;
+            }
         }
     }
 
@@ -303,7 +464,6 @@ public class DemoWWJ
         }
         layers.add(compassPosition, layer);
     }
-
 
     static
     {
@@ -349,7 +509,6 @@ public class DemoWWJ
             return null;
         }
     }
-
 
     public static void main(String[] args)
     {
